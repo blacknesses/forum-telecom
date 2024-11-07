0da598fb3a57890db883d377ac286f6c3ea8cba8
BLACKNESS <96961178+blacknesses@users.noreply.github.com>
2024-11-07 18:13:57 -0300

    script_otimizacao.sh

--- script_otimizacao.sh
+++ script_otimizacao.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# ATUALIZA KERNEL E GERENCIADOS DE PACOTES
-sudo apt-get update ; sudo apt-get full-upgrade -y ; sudo apt install -f -y ; sudo dpkg --configure -a ; sudo apt clean -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt remove -f -y && sudo apt-get -u dist-upgrade -y && sudo updatedb"
+# ATUALIZA KERNEL E GERENCIADOR DE PACOTES
+sudo apt-get update && sudo apt-get full-upgrade -y
+sudo apt install -f -y && sudo dpkg --configure -a
+sudo apt clean -y && sudo apt autoremove -y
+sudo apt autoclean -y && sudo apt remove -f -y
+sudo apt-get -u dist-upgrade -y && sudo updatedb
 
-# INSTALANDO DEPENDÊNCIAS
-sudo apt-get install -y mtr syslog
+# INSTALANDO DEPENDENCIAS
+sudo apt-get install -y mtr rsyslog
 
 # OTIMIZAÇÃO DA SWAP
+if grep -q '/swapfile' /etc/fstab; then
+    sudo swapoff /swapfile
+    sudo rm /swapfile
+fi
+
 sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -16,12 +25,25 @@ echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
 # OTIMIZAÇÃO DA CPU
-echo 'performance' | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+if [ -d "/sys/devices/system/cpu/cpu0/cpufreq" ]; then
+    echo 'performance' | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+else
+    echo "Configuração de 'scaling_governor' não disponível no sistema."
+fi
 
 # OTIMIZAÇÃO DO KERNEL
-echo '1' | sudo tee /sys/devices/system/cpu/cpu*/cpuidle/state3/disable
+if [ -d "/sys/devices/system/cpu/cpu0/cpuidle" ]; then
+    echo '1' | sudo tee /sys/devices/system/cpu/cpu*/cpuidle/state3/disable
+else
+    echo "Configuração de 'cpuidle' não disponível no sistema."
+fi
+
 echo 'nohz=on' | sudo tee -a /etc/default/grub
 sudo update-grub
 
 # OTIMIZAÇÃO DO DISCO
-sudo fstrim -v /
+if sudo fstrim -v / &> /dev/null; then
+    echo "Trim realizado com sucesso."
+else
+    echo "Trim não suportado nesta unidade."
+fi
