a010dae0865f59a8ab9be42d457ecad92360e36d
BLACKNESS <96961178+blacknesses@users.noreply.github.com>
2024-11-07 21:27:11 -0300

    script_otimizacao.sh

--- script_otimizacao.sh
+++ script_otimizacao.sh
@@ -10,12 +10,13 @@ sudo apt-get -u dist-upgrade -y && sudo updatedb
 # INSTALANDO DEPENDENCIAS
 sudo apt-get install -y mtr rsyslog
 
-# OTIMIZAÇÃO DA SWAP
+# REMOEVNDO SWAP DEFAULT
 if grep -q '/swapfile' /etc/fstab; then
     sudo swapoff /swapfile
     sudo rm /swapfile
 fi
 
+# CRIANDO SWAP OTIMIZADA
 sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -24,26 +25,27 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
+# OTIMIZAÇÃO DE RENDERIZAÇÃO
+echo 'nohz=on' | sudo tee -a /etc/default/grub
+sudo update-grub
+
 # OTIMIZAÇÃO DA CPU
 if [ -d "/sys/devices/system/cpu/cpu0/cpufreq" ]; then
     echo 'performance' | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 else
-    echo "Configuração de 'scaling_governor' não disponível no sistema."
+    echo "Erro: kernel não possui suporte de otimização de CPU [❌]"
 fi
 
 # OTIMIZAÇÃO DO KERNEL
 if [ -d "/sys/devices/system/cpu/cpu0/cpuidle" ]; then
     echo '1' | sudo tee /sys/devices/system/cpu/cpu*/cpuidle/state3/disable
 else
-    echo "Configuração de 'cpuidle' não disponível no sistema."
+    echo "Erro: kernel não possui suporte para otimização modular [❌]"
 fi
-
-echo 'nohz=on' | sudo tee -a /etc/default/grub
-sudo update-grub
 
 # OTIMIZAÇÃO DO DISCO
 if sudo fstrim -v / &> /dev/null; then
     echo "Trim realizado com sucesso."
 else
-    echo "Configuração de 'trim' não suportado nesta unidade."
+    echo "Erro: kernel não possui suporte para otimização de disco [❌]"
 fi
